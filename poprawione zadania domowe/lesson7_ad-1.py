a831899f11a178edd632c27c3d23db6c3b8c7f57
Ewelinaszymanska85 <27kwiecien1985@gmail.com>
2026-08-19 20:32:40 +0200

    Usuniecie duplikatow poprawionych zadan

--- poprawione zadania domowe/lesson7_ad-1.py
+++
@@ -1,5 +0,0 @@
-slowa = ["jabłko", "banan", "kiwi", "gruszka", "truskawka"]
-
-wynik = [slowo for slowo in slowa if len(slowo) > 5]
-
-print(wynik) 
--- poprawione zadania domowe/lesson7_ad-2.py
+++
@@ -1,5 +0,0 @@
-oceny = {"Jan": 4, "Anna": 5, "Piotr": 3, "Kasia": 4}
-
-posortowane = sorted(oceny.items(), key=lambda x: x[1], reverse=True)
-
-print(posortowane) 
